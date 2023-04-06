3429507cb7b2cf0fc8363109bd1fae337d2aae65
Canan Ağbal <agbalcanan8069@gmail.com>
2023-04-06 20:51:02 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import Header from "./components/Header"
 import "./scss/app.scss"
 import data from "./data"
 
-
 function App() {
   return (
     <>
       <Header />
-      <Card  data = {data} />
+      <Card data={data} /> {/* datayı bir bütün olarak gönderdik. data.js de dest ederek karşılayacağız*/}
       <Footer />
     </>
   )
 }
 
 export default App
+  
+
+// npm i sass  veya yarn add sass diyerek sass kütüphanisini terminale kuruyoruz.
+//yarn upgrade --latest dersek eklediğimiz kütüphanelerin yeni versiyonlarını güncellemiş oluruz. 
